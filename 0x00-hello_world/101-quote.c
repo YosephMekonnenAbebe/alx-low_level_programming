150ebe9e46b13066cd33944c2999f0b92418e911
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-02 15:32:25 -0700

    Task 8 Qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,10 +12,7 @@
 
 int main(void)
 {
-	FILE * fp;
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	fp = fopen("file.txt", "w");
-	fwrite(str, 2, sizeof(str), fp);
-	fclose(fp);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, str, 59);
 	return (1);
 }
