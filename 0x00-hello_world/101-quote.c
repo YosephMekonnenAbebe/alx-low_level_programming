0284534309e788a05343ae87a65f73c1ef787ecd
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-02 16:33:49 -0700

    Task 8 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
+#include <stdio.h>
 #include <unistd.h>
-
-/** main -starting point
-  * 
-  * Description: with write
-  * 
-  * Return: Always 1 (Success)
-  *
-  */
+/**
+ * main -start point
+ * Description:no print
+ *
+ * Return: return 1 means succes
+ */
 int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+
 	write(2, str1, 59);
 	return (1);
 }
