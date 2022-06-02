3b547bd49f1c1c2f7de4379f7b581a861ca35bcd
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-02 13:13:59 -0700

    Task 5 Print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *  main -The starting point of the program
+ *
+ *  Return: Always 0 indicates a succes
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
