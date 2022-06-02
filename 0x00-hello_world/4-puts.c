a76937bc281018cb75b7ad2ba4d528617b5633ee
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-02 12:24:55 -0700

    Task 4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
  *
  *  * main - Entry point of the code
@@ -9,11 +9,13 @@
  *    * Return: Always 0 (Success)
  *
  *     */
+
 int main(void)
 
 {
 
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
 
 }
