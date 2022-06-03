04d3ed8a93d1f28b5235c33968a23f1eec1235d4
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-03 15:09:27 -0700

    Task 5 base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: character desplay
+ * Return: 0 means succes
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		printf("%d", a);
+		a++;
+	}
+		printf("\n");
+
+	return (0);
+}
