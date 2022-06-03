562019ba6c31762bdaac7f748101d1188ccdea5c
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-03 13:08:00 -0700

    Alphabet display

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *
+ * main -starting point
+ *
+ * description character desplay
+ * Return: 0 means succes
+ */
+int main(void)
+{
+	char al = 'a';
+
+	while(al <= 'z')
+	{
+		putchar(al);
+		al++;
+	}
+
+	return (0);
+}
+
+
