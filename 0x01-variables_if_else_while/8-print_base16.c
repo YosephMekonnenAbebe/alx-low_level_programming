384f495176307492a1e87ef20b28add8e49ee519
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-03 16:16:34 -0700

    Task 8 hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Hexadecimal
+ * Return: 0 means succes
+ */
+int main(void)
+{
+	int a;
+
+	char al = 'a';
+	while (a <= 9)
+	{
+		putchar((a % 10)+ '0');	
+		a++;
+	}
+	while (al <= 'f')
+	{
+		putchar(al);
+		al++;
+	}
+	putchar('\n');
+
+	return (0);
+}
