8f342962a8f24d8f077e507a75cf1f0d7061303b
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-03 17:42:51 -0700

    Task 10 Two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <time.h>
+#include <stdlib.h> 
+#include <stdio.h> 
+/**
+ * main - Entry point
+ *
+ * Description: Two digit number
+ * Return: 0 means succes
+ */
+int main(void)
+{
+	int a = 0;
+
+	int c;
+	int d;
+
+	while (a <= 99)
+	{
+		c = ((a / 10) + '0');
+		d = ((a % 10) + '0');
+		if (c < d)
+		{
+			putchar(c);
+		        putchar(d);
+			if (a !=89)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+		++a;
+	}
+	putchar('\n');
+
+	return (0);
+}
