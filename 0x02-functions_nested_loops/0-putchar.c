ebc5b35a55a536b71f8f028c055117565230f8ce
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-07 14:31:25 -0700

    Adding putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main -Starting point
+ *
+ * Description :Function prototype
+ * Return 0 means succes
+ */
+int main(void)
+{
+	char str[8] = "_putchar";
+
+	int i = 0;
+
+	while(i < 8)
+	{
+		_putchar(str[i]);
+		++i;
+	}
+	_putchar('\n');
+
+	return (0);
+}
