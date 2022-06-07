4cd081cf692aac42196ab7f3f66913c89638b117
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-07 16:11:30 -0700

    Task 4:

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h" 
+/**
+ * _isalpha -starting point
+ * @c: a letter input
+ * Return: 1 if lower upper case, 0 if not
+ */
+int _isalpha(int c)
+{
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+	return (1);
+	else
+	return (0);
+}
