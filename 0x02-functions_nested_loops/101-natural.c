065353ed43b4ebc442c940c93075ec5f8d85d1dc
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-08 10:59:45 -0700

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "<main.h"
+/**
+ * main -start point
+ * Description: sum
+ * Return: o means succes
+ */
+int main ()
+{
+	int i = 0;
+	int sum = 0;
+	for (i = 0; i<1024; i++)
+	{
+	if (i % 3 == 0 || i % 5 == 0)
+	sum =sum + i;
+	_putchar(sum + '0');
+	}
+	_putchar('\n');
+}
