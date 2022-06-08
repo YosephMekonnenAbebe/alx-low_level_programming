35b614d15f31891c44a18a2aa0369dde0bbdcb99
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-08 01:23:08 -0700

    Task1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,14 +12,14 @@ void jack_bauer(void)
 	
 	for (a = 0; a < 24; a+)
 	{
-		for (b = 0; b < 60; b++)
-		{
-		_putchar((a / 10) + '0');
-		_putchar((a % 10) + '0');
-		_putchar(':');
-		_putchar((b / 10) + '0');
-		_putchar((b % 10) + '0');
-		_putchar('\n');
-		}
+	for (b = 0; b < 60; b++)
+	{
+	_putchar((a / 10) + '0');
+	_putchar((a % 10) + '0');
+	_putchar(':');
+	_putchar((b / 10) + '0');
+	_putchar((b % 10) + '0');
+	_putchar('\n');
+	}
 	}
 }
