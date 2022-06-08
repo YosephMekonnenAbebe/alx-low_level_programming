fc9e54689c184cbe5b4ce69670c78bd11ae3e879
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-08 11:10:21 -0700

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,16 +6,18 @@
  * Description: sum
  * Return: o means succes
  */
-int main ()
+int main (void)
 {
 	int i = 0;
 	int sum = 0;
-	for (i = 0; i<1024; i++)
+
+	for (i = 0; i < 1024; i++)
 	{
 	if (i % 3 == 0 || i % 5 == 0)
+	{
 	sum =sum + i;
-	_putchar(sum + '0');
 	}
+	_putchar(sum + '0');
 	_putchar('\n');
 	
 	return (0);
