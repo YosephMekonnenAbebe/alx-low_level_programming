ed0bc543b69502617aded95d1338ebe5c6d9f086
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-07 17:49:24 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,12 +8,12 @@
  */
 void jack_bauer(void)
 {
-	int a = 0;
-	int b = 0;
+	int a, b;
 	
-	while (a < 24)
+	for (a = 0; a < 24; a++)
 	{
-	while (b < 60)
+	for (b = 0; b < 60; b++)
+	{
 	{
 	_putchar((a / 10) + '0');
 	_putchar((a % 10) + '0');
@@ -21,8 +21,6 @@ void jack_bauer(void)
 	_putchar((b / 10) + '0');
 	_putchar((b % 10) + '0');
 	_putchar('\n');
-	++b;
 	}
-	++a;	
-	}
+		}
 }
