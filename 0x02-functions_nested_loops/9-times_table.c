01a44108f38d63c835a714988997a6d3a57b983d
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-08 15:29:51 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,10 +12,16 @@ void times_table(void)
 	{
 	for (j = 0; j <= 9; j++)
 	{
-		k = j * i; 
+		k = j * i;
+	       if (k < 10)
+	       {
+		_putchar((k % 10) + '0');  
+	       }
+	       else
+	       {	       
 		_putchar((k / 10) + '0');
 		_putchar((k % 10) + '0');
-		
+	       }
 		if (k <= 9)
 		{
 			if (j !=9)
