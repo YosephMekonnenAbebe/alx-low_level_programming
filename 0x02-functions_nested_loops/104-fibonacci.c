1efeffaa0d83ad1ba5ffc0c86d16e0ab2e4f2901
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-08 14:05:12 -0700

    Task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+/**
+ * main -starting point
+ * description: fibonaci
+ * Return: 0 means succes
+ */
+int main (void)
+{
+	int num;
+	unsigned long f1 = 0, f2 = 1, sum;
+	unsigned long ff11, ff12, ff21, ff22;
+	unsigned long ff1, ff2;
+
+	for (num = 0; num < 92; num++)
+	{
+		sum = f1 + f2;
+		printf("%lu, ", sum);
+
+		f1 = f2;
+		f2 = sum;
+	}
+	ff11 = f1 / 10000000000;
+	ff21 = f2 / 10000000000;
+	ff12 = f1 % 10000000000;
+	ff22 = f2 % 10000000000;
+
+	for (num = 93; num < 99; num++)
+	{
+		ff1 = ff11 + ff21;
+		ff2 = ff12 + ff22;
+		if (ff12 + ff22 > 9999999999)
+			ff1 = ff1 + 1;
+			ff2 = ff2 % 10000000000;
+	}
+
+	printf("%lu%lu", ff1, ff2);
+	if (num != 98)
+		printf(", ");
+
+	ff11 = ff21;
+	ff12 = ff22;
+	ff21 = ff1;
+	ff22 = ff2;
+}
+printf("\n");
+return (0);
+}
