90e1572d93770827e3f8a3f5c054026505feeb31
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-07 18:04:04 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+nclude "main.h"
+/**
+ * times_table - starting point
+ * rn = row,
+ * Return: Table
+ */
+void times_table(void)
+{
+	int rn, cn, d;
+	
+	for (rn = 0; rn <= 9; rn++)
+	{
+	_putchar('0');
+	_putchar(',');
+	_putchar(' ');
+	for (cn = 1; cn <= 9; cn++)
+	{
+	d = (rn * cn);
+	if ((d / 10) > 0)
+	{
+	_putchar((d / 10) + '0');
+	}
+	else
+	{
+		_putchar(' ');
+	}
+	if (cone < 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	}
+	_putchar('\n');
+	}
+}
+
