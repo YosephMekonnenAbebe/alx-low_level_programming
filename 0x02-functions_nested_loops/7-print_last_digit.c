453a81c167e8ad5eb2822ad391e29a98700a4a8b
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-07 17:13:17 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,17 +5,16 @@
  * Return: modulo of a number
  */
 int print_last_digit(int a)
-{ 
+{
 	int mod;
 
 	mod = (a % 10);
-	
+
 	if (mod < 0)
 	{
 	mod = (-mod);
 	}
 	_putchar(mod + '0');
-	return (mod);
-
 	
+	return (mod);
 }
