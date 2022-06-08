c0fdf0c5cdbad4c3a0c7247b05f0e3993a950d9f
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-08 16:57:47 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,38 +7,36 @@
 void times_table(void)
 {
 	int k, i, j;
-
+	
 	for (i = 0; i <= 9; i++)
 	{
 	for (j = 0; j <= 9; j++)
 	{
-		k = j * i;
-	       if (k < 10)
-	       {
-		_putchar((k % 10) + '0');
-	      if((j != 9) && (i * (j+1) < 10))
-	      {
-		      _putchar(',');
-		      _putchar(' ');
-		      _putchar(' ');
-	      }
-	      else if ((j != 9) && (i * (j+1) >= 10))
-	      {
-		     _putchar(',');
-		    _putchar(' ');
-	      } 
-	       }
- 	       else
-	       {	       
-		_putchar((k / 10) + '0');
-		_putchar((k % 10) + '0');
-
-		if (j != 9)
-		{
-			_putchar(',');
-			_putchar(' ');
-	       }
-	       }
+	k = j * i;
+	if (k < 10)
+	{
+	_putchar((k % 10) + '0');
+	if((j != 9) && (i * (j + 1) < 10)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	}
+	else if ((j != 9) && (i * (j + 1) >= 10))
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	else
+	{
+	_putchar((k / 10) + '0');
+	_putchar((k % 10) + '0');
+	if (j != 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	}
 	}
 	_putchar('\n');
 	}
