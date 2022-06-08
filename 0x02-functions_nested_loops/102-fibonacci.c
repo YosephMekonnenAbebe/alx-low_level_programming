7e9d4e42768d3768480ce177c0723292f274d4b6
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-08 13:06:41 -0700

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+nclude <stdio.h>
+/**
+ * main -starting point
+ * description: Fibonaci
+ * Return: 0 means succes
+ */
+int main(void)
+{
+	int c;
+	unsigned long f1 = 0, f2 = 1, sum;
+
+	for (c = 0; c < 50; c++)
+	{
+		sum = f1 + f2;
+		printf("%lu", sum);
+
+		f1 = f2;
+		f2 = sum;
+
+		if (c == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
