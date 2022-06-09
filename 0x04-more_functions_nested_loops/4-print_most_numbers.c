7cafbbe0c57227bed380053bcbd91b00d5fc1e84
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-09 12:59:48 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -starting point
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		if (num != 2 && num != 4)
+			_putchar(num + '0');
+		num++;
+	}
+	_putchar('\n');
+}
