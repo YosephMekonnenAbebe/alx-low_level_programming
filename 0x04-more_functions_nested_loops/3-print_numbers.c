7cdf8c8ad8d86e4a52c66588c0722ba9a875467a
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-09 12:29:08 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers -starting point
+ * Description: prints a number
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int a = 0;
+
+	if (a <= 9)
+	{
+		_putchar(num + '0');
+		a++;
+	}
+	_putchar('\n');
+}
