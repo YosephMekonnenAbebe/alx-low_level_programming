b082f823dd4977030c4e1dfb1449c6bc78ce5d0c
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-09 12:19:08 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul -starting point
+ * @a: first element
+ * @b: second element
+ * Desription: a*b
+ * Return: mult of int
+ */
+
+int mul(int a, int b)
+{
+	int as = a * b;
+
+	return (as);
+
+}
