d8e3772a2526b7dfcd612e612a1c464fa6d12a1a
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-09 18:36:51 -0700

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,18 +11,18 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else
 	{
-		for (co = 1; co <= n; co++)
-		{
-			for (sp = 1; sp < co; sp++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
+	for (co = 1; co <= n; co++)
+	{
+	for (sp = 1; sp < co; sp++)
+	{
+	_putchar(' ');
+	}
+	_putchar('\\');
+	_putchar('\n');
+	}
 	}
 }
