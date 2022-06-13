0ab90e964992be3a61b6ed7f8df78ca45bf02718
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-13 11:06:02 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_rev -the starting point
+ * Description: reverse string
+ * @s: the input argument
+ * Return: string in reverse
+ */
+void print_rev(char *s)
+{
+	int count =0;
+	int i = 0;
+	int j ;
+
+	while (s[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+	
+	j = count - 1;
+
+	while ( j >= 0)
+	{
+		_putchar(s[j]);
+		j--;
+	}
+	_putchar('\n');
+}
+
