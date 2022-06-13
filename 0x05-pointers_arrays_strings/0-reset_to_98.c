d4990b4d20c4bf8efe47072e8cc7194e38e005e2
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-13 08:58:09 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 -the starting of a function
+ * Description: updates the value
+ * @n: the input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pointer Arrray and Strings
