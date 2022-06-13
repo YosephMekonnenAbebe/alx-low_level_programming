5fbde98b564a0a72ca27aa363bfb495be93192ef
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-13 13:46:20 -0700

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -starting of the program
+ * Description: pass word
+ *
+ * Return: always 0 for succes
+ */
+
+int main(void)
+{
+	int a = 0;
+	int b, sum;
+	int pw[100];
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	while (a < 100)
+	{
+		pw[a] = rand() % 78;
+		sum += (pw[a] + '0');
+		putchar(pass[a] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			b = 2772 - sum - '0';
+			sum += b;
+			putchar(b + '0');
+			break;
+		}
+		a++;
+	}
+
+	return (0);
+}
+
+
