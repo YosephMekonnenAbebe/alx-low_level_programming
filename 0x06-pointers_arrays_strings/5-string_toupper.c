185020f77c9bf29a24c9570d96364f16649949e7
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-16 11:53:07 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - starting point
+ * @str: input one
+ *
+ * Return: succes is the return
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i =0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+	}
+
+	return (str);
+}
