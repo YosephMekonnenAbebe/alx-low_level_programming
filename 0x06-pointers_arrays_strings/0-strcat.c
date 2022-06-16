b3b6ae827ee38cf38fe8899c0a0b7f0316fdb09c
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-16 12:43:34 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int i;
-int j;
-
-i = 0;
+	int i = 0;
+	int j = 0;
 
 while (dest[i] != 0)
 {
-i++;
+	i++;
 }
-
-j = 0;
 
 while (src[j] != 0)
 {
-dest[i] = src[j];
-i++;
-j++;
+	dest[i] = src[j];
+	i++;
+	j++;
 }
 
 return (dest);
