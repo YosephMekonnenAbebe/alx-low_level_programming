ae0ada83e96f51d88dee844365d25774fcbeabc5
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-16 00:21:41 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h" 
+
+/**
+ * main -Enter point
+ *
+ * Description: string concatinate
+ * @dest: input1
+ * @src
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	dest[i] = ' ';
+
+for (j = 0; src[j] !='\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	return(dest);
+}
+
