23c4fc3da183520c397aa6342a69fdf0e0f86d58
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-16 15:14:25 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-       
 	i = 0;
+
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
 
-	i = i;
+	i = ii;
 	while (i < n)
 	{
 		dest[i] = '\0';
