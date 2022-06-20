1ea80abbe323a196554874e92c3ead76aab71298
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-20 15:26:02 -0700

    Task 5

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_chessboard - print
+ * @a: The character
+ *
+ * Return: nothing
+ */
+void print_chessboard(char(*a)[8])
+{
+	int x = 0;
+	int y = 0;
+
+	while (x < 8)
+	{
+		while (y < 8)
+		{
+			_putchar(a[x][y]);
+			y++;
+		}
+		x++;
+	}
+
