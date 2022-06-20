6873f8bea520517c2ee8afe6e027ae13c10cce90
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-20 14:12:33 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strchr - starting function
+ * @s: Pointer1
+ * @c: Character input
+ *
+ * Return: has a return
+ */
+char *_strchr(char *s, char c)
+{
+	int num = 0;
+
+	for (num = 0; s[num] != '\0'; num++)
+		if(s[num] == c)
+			return(&s[num]);
+	return ('\0');
+}
