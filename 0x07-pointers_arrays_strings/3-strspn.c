d556cc295655ca5b7d7e7c5dea62dd241be26871
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-20 14:49:51 -0700

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strspn - gets the length
+ * @s: pointer
+ * @accept: pointer
+ *
+ * Return: the number
+ */
+unsigned int _strspn(char *s, char *accept)
+	int leng = 0;
+	int x = 0;
+	int y = 0;
+
+	while (s[x] != '\0')
+{
+	while (accept[y] != '\0')
+	{
+		if (accept[y] == s[x])
+		{
+			leng++;
+		}
+		y++;
+	}
+	y = 0;
+	x++;
+	if (s[x] == ' ')
+	{
+		break;
+	}
+}
+return (leng);
+}
