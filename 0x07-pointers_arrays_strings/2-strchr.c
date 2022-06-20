310eebbc772e08164ed28ab335e0551c2d8d724d
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-20 14:19:55 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,10 +8,10 @@
  */
 char *_strchr(char *s, char c)
 {
-	int num = 0;
+int num = 0;
 
-	for (num = 0; s[num] != '\0'; num++)
-		if (s[num] == c)
-			return (&s[num]);
-	return ('\0');
+for (num = 0; s[num] != '\0'; num++)
+if (s[num] == c)
+return (&s[num]);
+return ('\0');
 }
