18e0e7ffe96e38875c51da8c2ebd366a37464eeb
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-20 13:42:28 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset -starting of a function
+ * @s: pointer
+ * @n: intiger input
+ * @b: character input
+ *
+ * Return: the area of s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int num;
+
+	for (num = 0; num < n; num++)
+		s[num] = b;
+}
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pointer Aray and Strings
