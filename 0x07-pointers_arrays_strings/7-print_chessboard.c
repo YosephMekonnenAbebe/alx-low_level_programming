8012c28ce667323aa28cb402055b43f2a239c71e
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-20 15:47:53 -0700

    Task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,17 +6,15 @@
  */
 void print_chessboard(char(*a)[8])
 {
-	int x = 0;
-	int y = 0;
+	int x;
+	int y;
 
-	while (x < 8)
+	for (x = 0; x < 8; x++)
 	{
-		while (y < 8)
+		for (y = 0; y < 8: y++)
 		{
 			_putchar(a[x][y]);
-			y++;
 		}
 		_putchar('\n');
-		x++;
 	}
 }
