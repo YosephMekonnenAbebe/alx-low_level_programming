17f255ca9df973dc9e6272b46683a32152ca1ca5
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-22 13:25:16 -0700

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * factorial - starting point
+ * @n: input
+ *
+ * Return: intiger value
+ */
+int factorial(int n)
+{
+	if (n < 0 || n == 0)
+	{
+		if(n < 0)
+			return (-1);
+		else
+			return (1);
+	}
+	else
+		return (n * factorial(n - 1));
+}
+		
