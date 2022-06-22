b3ccac28ac39a03a74a9e5585ef0be49864ce475
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-22 13:10:36 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - starting point
+ * @s: input
+ *
+ * Return: valued int
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
