8a06a4dba58498525e7794014877a793fc933cfd
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-22 15:07:51 -0700

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+int primenum(int n, int num);
+/**
+ * is_prime_number - Starting point
+ * @n: input
+ *
+ * Return: succed
+ */
+int is_prime_number(int n)
+{
+	return (primenum(n, 1);
+}
+/**
+ * primenum - start poinyt
+ * @n: input
+ * @num: input two
+ *
+ * Return: 1 for succes
+ */
+int primenum(int n, int num)
+{
+if ( n<= 1)
+return (0);
+if (n % num == 0 && num >1)
+return (0);
+if ((n / num) < num)
+return (1);
+return (primenum(n, num+1));
+}
