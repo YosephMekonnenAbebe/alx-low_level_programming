37c476e19d95fb2abc253b9b1a68434f8d1bf5b8
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-24 16:59:41 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Program that
+ * @argc: Number 
+ * @argv: Arra
+ * Return: 1 if succes
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		j = 1;
+		 for (i = 1; i < 3; i++)
+			 j *= atoi(argv[i]);
+
+		 printf("%d\n", j);
+	}
+
+	return (0);
+}
