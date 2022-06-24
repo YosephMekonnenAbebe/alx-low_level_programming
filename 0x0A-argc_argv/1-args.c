998d408c72a00dd45ca7ffa4e9e23dc725599e56
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-24 16:30:47 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - displays the no of passed args
+ * @argc: Number
+ * @argv: Array of names
+ * Return: 0 is a succes
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int m = 0;
+	
+	while (m < argc)
+	{
+		m++;
+	}
+	printf("%d\n", m - 1);
+	return (0);
+}
