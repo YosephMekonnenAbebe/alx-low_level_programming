956404939194c7093fd05481cfb96c61e069d77c
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-24 16:07:23 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Printing a name
+ * @argc: Comand lines number
+ * @argv: name
+ * Return: succes
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
