69f7435ed71bfe32b59ad6b4a8d61b60c9f63822
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-24 18:28:18 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,7 +13,6 @@ int main(int argc, char *argv[])
 {
 int i, j, length, sum;
 char *ptr;
-
 if (argc < 2)
 printf("0\n");
 else
@@ -23,7 +22,6 @@ for (i = 1; i < argc; i++)
 {
 ptr = argv[i];
 length = strlen(ptr);
-
 for (j = 0; j < length; j++)
 {
 if (isdigit(*(ptr + j)) == 0)
@@ -32,11 +30,9 @@ printf("Error\n");
 return (1);
 }
 }
-
 sum += atoi(argv[i]);
 }
-
 printf("%d\n", sum);
 }
-return (0)
-}	  
+return (0);
+}
