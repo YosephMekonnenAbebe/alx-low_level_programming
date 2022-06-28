bd650c7d0d568f8e0e75826ee9f4b7672e413f3c
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-28 08:21:51 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 /**
+ * create_array - Entry point
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
