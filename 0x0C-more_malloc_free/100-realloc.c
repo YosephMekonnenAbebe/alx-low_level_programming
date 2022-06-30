b49f5a6ae5a39c7348eb695f8e4de9c40c2ce518
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-30 13:09:52 -0700

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,7 +19,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	if (ptr == NULL)
 	{
-		return (NULL);
+		ptr1 = malloc(new_size);
+		if (ptr1 == NULL)
+			return (NULL);
 		return (ptr1);
 	}
 	else
