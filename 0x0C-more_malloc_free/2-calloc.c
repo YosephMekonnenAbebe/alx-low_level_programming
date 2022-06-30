36ec9ad60123e5970fb1df4e880cc4ebdf258ecd
Yoseph Mekonnen <yosephgod@gmail.com>
2022-06-30 11:45:30 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,21 +12,21 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr1;
 	char *ptr2;
-	int append = 0;
+	unsigned int append = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	ptr1 = malloc(size * nmemb);
 
-	if (ptr1 = NULL)
+	if (ptr1 == NULL)
 		return (NULL);
 
 	ptr2 = ptr1;
 
 	while (append < (size * nmemb))
 	{
-		ptr2[append];
+		ptr2[append] = '\0';
 		append++;
 	}
 
