bfe170cec0e43bf6f7692b966590eabb0ec24e2b
Yoseph Mekonnen <yosephgod@gmail.com>
2022-07-04 13:03:06 -0700

    Task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,16 +11,15 @@
 typedef struct dog
 {
 	char *name;
-	double age;
+	float age;
 	char *owner;
 }
-dog_t;
-
-void init_dog(struct dog *d, char *name, double age, char *owner);
-
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+int _strlen(char *str);
 void print_dog(struct dog *d);
-
-dog_t *new_dog(char *name, double age, char *owner);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
 
