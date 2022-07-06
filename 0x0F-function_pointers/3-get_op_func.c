2bac58dd016208d4b075166d5d49fab1e901e9c3
Yoseph Mekonnen <yosephgod@gmail.com>
2022-07-06 13:48:20 -0700

    Task ~

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,16 +17,16 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int k;
+	int i;
 
-	k = 0;
-	while (k < 5)
+	i = 0;
+	while (i < 5)
 	{
-		if (*s == *(ops[k]).op)
+		if (*s == *(ops[i]).op)
 		{
-			return (*(ops[k]).f);
+			return (*(ops[i]).f);
 		}
-		k++;
+		i++;
 	}
 	return (NULL);
 }
