3aa97be743b87afc99959b62b631c8b912445c94
Yoseph Mekonnen <yosephgod@gmail.com>
2022-07-06 07:57:49 -0700

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function parameter.
+ * @array: pointer to integer array.
+ * @size:  size of an array.
+ * action: a pointer to the function you need to use.
+ * Return: nothing to return.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t szi;
+	
+	if (action != NULL && array != NULL)
+	{
+		for (szi = 0; szi < size; szi++)
+			action(array[szi]);
+	}
+}
