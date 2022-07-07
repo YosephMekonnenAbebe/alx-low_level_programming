bf0ae29dffbad3432ab00fb83f3ff172094b8ab0
Yoseph Mekonnen <yosephgod@gmail.com>
2022-07-07 12:20:37 -0700

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,10 +16,10 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	va_start(ap, n);
+	va_start(a, n);
 	for (; i < n; i++)
-		sum += va_arg(ap, int);
+		sum += va_arg(a, int);
 	
-	va_end(ap);
+	va_end(a);
 	return (sum);
 }
