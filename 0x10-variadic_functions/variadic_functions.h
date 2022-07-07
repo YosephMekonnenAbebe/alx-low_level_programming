a9b97c7f1debcdbe65ea98fd1debdd64bad3d5c4
Yoseph Mekonnen <yosephgod@gmail.com>
2022-07-07 12:24:09 -0700

    Task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,7 +7,7 @@ int _putchar(char c);
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-oid print_strings(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void print_char(va_list);
 void print_int(va_list);
