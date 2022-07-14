5a5be0168b9145025defc9d5573695357074c728
Yoseph Mekonnen <yosephgod@gmail.com>
2022-07-14 14:02:41 -0700

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees the lists needed
+ * @head: pointer used for the struct
+ */
+void free_list(list_t *head)
+{
+list_t *buf_buf;
+
+while (head != NULL)
+{
+buf_buf = head;
+head = head->next;
+free(buf_buf->str)
+free (buf_buf);
+}
+}
