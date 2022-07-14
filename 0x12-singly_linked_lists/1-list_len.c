8c5dae96f4226a08d13bf9320ff31494a97b48cd
YosephMekonnenAbebe <yosephgod@gmail.com>
2022-07-14 23:33:12 +0300

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - element finder
+ * @h: the starter of the linked list pointer
+ *
+ * Return: counted elements
+ */
+size_t list_len(const list_t *h)
+{
+const list_t *emper;
+unsigned int idia = 0;
+
+emper = h;
+whiel (emper)
+{
+emper = emper->next;
+idia++;
+}
+return (idia);
+}
