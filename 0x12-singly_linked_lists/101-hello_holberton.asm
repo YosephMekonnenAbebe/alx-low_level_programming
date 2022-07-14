cd96ef884e7269b068cd622b5c3993bdba404854
Yoseph Mekonnen <yosephgod@gmail.com>
2022-07-14 15:53:46 -0700

    Task 5

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,16 @@
-	global  main	
-	extern 	printf
-	
-	section .text
+SECTION .data
+msg:		    db "Hello, Holberton", 0
+fmt:		    db "%s", 10, 0
+
+SECTION .text
+extern printf
+global main
 main:
-	mov  rdi, format
-	mov  rax, 0
-	call  printf
-	mov  rax, 0
-	ret
-format:
-	db "Hello, Holberton", 0
+mov esi, msg
+mov edi, fmt
+mov eax, 0
+call printf
+
+
+mov eax, 0
+ret
