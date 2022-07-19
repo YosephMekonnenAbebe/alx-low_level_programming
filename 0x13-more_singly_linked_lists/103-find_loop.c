cf58130e7213dca63a66e0cb98d7de3b5236300e
Yoseph Mekonnen <yosephgod@gmail.com>
2022-07-19 13:13:37 -0700

    Task 103

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - Find
+ * @head: A pointer
+ * Return: one or zero
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tort, *har;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	tort = head->next;
+	har = (head->next)->next;
+
+	while (har)
+	{
+		if (tort == har)
+		{
+			tort = head;
+
+			while (tort != har)
+			{
+				while (tort != har)
+					har = har->next;
+			}
+
+			return (tort);
+		}
+
+		tort = tort->next;
+		har = (har->next)->next;
+	}
+
+	return (NULL);
+}
