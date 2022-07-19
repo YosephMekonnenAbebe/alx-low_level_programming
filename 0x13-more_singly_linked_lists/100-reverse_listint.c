8a132090fbbc8e9cc08eac0e7231c3274f5adbb7
Yoseph Mekonnen <yosephgod@gmail.com>
2022-07-19 09:56:54 -0700

    Task 100

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverses 
+ * @head: A pointer 
+ * Return: A pointer
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *forwarding, *backwarding;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	backwarding = NULL;
+
+	while ((*head)->next != NULL)
+	{
+		forwarding = (*head)->next;
+		(*head)->next = backwarding;
+		backwarding = *head;
+		*head = forwarding;
+	}
+
+	(*head)->next = backwarding;
+
+	return (*head);
+}
