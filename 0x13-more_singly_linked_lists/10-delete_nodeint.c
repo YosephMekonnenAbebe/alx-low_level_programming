2323af31fd5e1935058240c158ab4b8022313f94
Yoseph Mekonnen <yosephgod@gmail.com>
2022-07-19 09:48:56 -0700

    Task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Deletes
+ * @head: A pointer
+ * @index: The index 
+ *
+ * Return:  negative 1 or positive
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *buff, *copy_node = *head;
+	unsigned int inode;
+
+	if (copy_node == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(copy_node);
+		return (1);
+	}
+
+	for (inode = 0; inode < (index - 1); inode++)
+	{
+		if (copy_node->next == NULL)
+			return (-1);
+
+		copy_node = copy_node->next;
+	}
+
+	buff = copy_node->next;
+	copy_node->next = buff->next;
+	free(buff);
+	return (1);
+}
