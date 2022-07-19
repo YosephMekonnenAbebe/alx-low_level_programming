689d89c350e2129ed5359f27f3505cfb2d3b8f02
Yoseph Mekonnen <yosephgod@gmail.com>
2022-07-19 13:37:58 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 
 	if (idx == 0)
-	{
+	{	new_node->next = copy_node;
 		*head = new_node;
 		return (new_node);
 	}
