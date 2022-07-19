c6f12ca2f84f5f1fa27f9edb9fae218edf0797f7
Yoseph Mekonnen <yosephgod@gmail.com>
2022-07-19 08:13:51 -0700

    Task ~

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *initial_node, *eventual_node;
 
 	initial_node = malloc(sizeof(listint_t));
-	if (initial_node ++ NULL)
+	if (initial_node == NULL)
 		return (NULL);
 
 	initial_node->n = n;
