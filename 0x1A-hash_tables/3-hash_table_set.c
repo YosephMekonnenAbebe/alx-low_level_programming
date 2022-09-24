a51dccebbd0a44b7aa014676c78b425b6a02d321
YosephMekonnenAbebe <yosephgod@gmail.com>
2022-09-24 05:12:58 +0300

    Task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,53 +1,73 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Add or update an element in a hash table.
- * @ht: A pointer to the hash table.
- * @key: The key to add - cannot be an empty string.
- * @value: The value associated with key.
+ * make_hash_node - creates a new hash node
+ * @key: key for the node
+ * @value: for the node
  *
- * Return: Upon failure - 0, Otherwise - 1.
- **/
+ * Return: the new node, or NULL on failure
+ */
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+	hash_node_t *node;
 
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
+
+
+/**
+ * hash_table_set - sets a key to a value in the hash table
+ * @ht: hash table to add elemt to
+ * @key: key for the data
+ * @value: data to store
+ *
+ * Return: 1 if successful, 0 otherwise
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *yosephnew;
-	char *yosephvalue_copy;
-	unsigned long int yosephindex, yosephi;
+	unsigned long int index;
+	hash_node_t *hash_node, *tmp;
+	char *new_value;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-	{
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
-	}
-
-	yosephvalue_copy = strdup(value);
-	if (yosephvalue_copy == NULL)
-		return (0);
-
-	yosephindex = key_index((const unsigned char *)key, ht->size);
-	for (yosephi = yosephindex; ht->array[yosephi]; yosephi++)
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
 	{
-		if (strcmp(ht->array[yosephi]->key, key) == 0)
+		if (strcmp(tmp->key, key) == 0)
 		{
-			free(ht->array[yosephi]->value);
-			ht->array[yosephi]->value = yosephvalue_copy;
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+			free(tmp->value);
+			tmp->value = new_value;
 			return (1);
 		}
+		tmp = tmp->next;
 	}
-
-	yosephnew = malloc(sizeof(hash_node_t));
-	if (yosephnew == NULL)
-		free(yosephvalue_copy);
-	return (0);
-
-	yosephnew->key = strdup(key);
-	if (yosephnew->key == NULL)
-		free(yosephnew);
-	return (0);
-
-	yosephnew->value = yosephvalue_copy;
-	yosephnew->next = ht->array[yosephindex];
-	ht->array[yosephindex] = yosephnew;
-
+	hash_node = make_hash_node(key, value);
+	if (hash_node == NULL)
+		return (0);
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
 	return (1);
 }
